fa34cdd633bbc0675bc6ae3979cd3edcd1ffe495
unknown <raselpinon@gmail.com>
2023-10-20 11:56:44 +0600

    Initial commit

---
+++ ProdctandCatagory/ProdctandCatagory/Controllers/CatagoryController.cs
@@ -0,0 +1,80 @@
+﻿using ProdctandCatagory.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProdctandCatagory.Controllers
+{
+    public class CatagoryController : Controller
+    {
+        // GET: Catagory
+        public ActionResult CatagoryView()
+        {
+            var db = new ProductandCatagoryEntities1();
+            var data=db.Catagories.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult CreateCatagory() 
+        {
+            var db = new ProductandCatagoryEntities1();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCatagory(Catagory catagory)
+        {
+            var db = new ProductandCatagoryEntities1();
+            bool catagoryExists = db.Catagories.Any(p => p.Name == catagory.Name);
+            if (catagoryExists)
+            {
+                ModelState.AddModelError("Name", "A Catagory with the same name already exists.");
+                return View(catagory);
+            }
+            db.Catagories.Add(catagory);
+            db.SaveChanges();
+            return RedirectToAction("CatagoryView");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ProductandCatagoryEntities1();
+            var res = db.Products.Find(id);
+            return View(res);
+        }
+        [HttpPost]
+        public ActionResult Edit(Catagory catagory)
+        {
+            var db = new ProductandCatagoryEntities1();
+
+            bool catagoryExists = db.Catagories.Any(p => p.Name == catagory.Name);
+            if (catagoryExists)
+            {
+                ModelState.AddModelError("Name", "A Catagory with the same name already exists.");
+                return View(catagory);
+            }
+            var exdata = db.Catagories.Find(catagory.Id);
+            exdata.Name = catagory.Name;
+            db.SaveChanges();
+
+            return RedirectToAction("CatagoryView");
+        }
+        public ActionResult Delete(int id)
+        {
+            var db = new ProductandCatagoryEntities1();
+            var exdata = db.Catagories.Find(id);
+            var productsToDelete = db.Products.Where(p => p.Cid == id);
+            db.Products.RemoveRange(productsToDelete);
+
+            if (exdata != null)
+            {
+                db.Catagories.Remove(exdata);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("CatagoryView");
+        }
+
+    }
+}
---
+++ ProdctandCatagory/ProdctandCatagory/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using ProdctandCatagory.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProdctandCatagory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult ProductView()
+        {
+            var db = new ProductandCatagoryEntities1();
+            var data=db.Products.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            var db = new ProductandCatagoryEntities1();
+            ViewBag.Catagories = new SelectList(db.Catagories, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Product product) 
+        {
+            var db = new ProductandCatagoryEntities1();
+            bool productExists = db.Products.Any(p => p.Name == product.Name);
+            if (productExists)
+            {
+                ModelState.AddModelError("Name", "A product with the same name already exists.");
+                return View(product);
+            }
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductView");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db=new ProductandCatagoryEntities1();
+            var res = db.Products.Find(id);
+            if (res != null)
+            {
+                // Find the associated category by Id
+                var category = db.Catagories.FirstOrDefault(c => c.Id == res.Cid);
+
+                if (category != null)
+                {
+                    ViewBag.CategoryName = category.Name;
+                }
+                else
+                {
+                    ViewBag.CategoryName = "Category Not Found";
+                }
+            }
+
+            return View(res);
+
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ProductandCatagoryEntities1();
+            var res = db.Products.Find(id);
+            ViewBag.Catagories = new SelectList(db.Catagories, "Id", "Name");
+            return View(res);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            var db = new ProductandCatagoryEntities1();
+
+            bool productExists = db.Products.Any(p => p.Name == product.Name);
+            if (productExists)
+            {
+                ModelState.AddModelError("Name", "A product with the same name already exists.");
+                return View(product);
+            }
+            var exdata = db.Products.Find(product.Id);
+            exdata.Name = product.Name;
+            exdata.Cid = product.Cid;
+            db.SaveChanges();
+
+            return RedirectToAction("ProductView");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var db = new ProductandCatagoryEntities1();
+            var exdata = db.Products.Find(id); 
+            db.Products.Remove(exdata);
+            db.SaveChanges();
+            return RedirectToAction("ProductView");
+        }
+
+
+
+
+    }
+}
