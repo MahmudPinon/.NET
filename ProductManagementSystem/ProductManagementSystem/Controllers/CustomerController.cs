5e4fe8ed4017c379922ff7792aa8b7aa3bad6e34
unknown <raselpinon@gmail.com>
2023-11-21 22:29:21 +0600

    Rasel Commit

---
+++ ProductManagementSystem/ProductManagementSystem/Controllers/CustomerController.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using ProductManagementSystem.EF;
+using ProductManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagementSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult BuyProduct()
+        {
+            var db = new ProductManagementEntities();
+            var product=db.Products.ToList();
+            var catagory = db.Catagories.ToList();
+            int customerId = int.Parse(Request.Cookies["CustomerInfo"]["CustomerId"]);
+            ViewBag.CustomerId = customerId;
+            return View(product);
+        }
+        [HttpPost]
+        /* public ActionResult BuyProduct(List<SelectedProduct> selectedProducts)
+         {
+             int customerId = 0;
+             if (Request.Cookies["CustomerInfo"] != null)
+             {
+                 customerId = int.Parse(Request.Cookies["CustomerInfo"]["CustomerId"]);
+             }
+             var selectedProduct = new List<SelectedProduct>();
+             if (selectedProducts != null && selectedProducts.Any())
+             {
+                 DateTime orderDate = DateTime.Now;
+
+                 using (var db = new ProductManagementEntities1())
+                 {
+                     var order = new Order
+                     {
+                         CustomerId = customerId,
+                         Date = orderDate,
+                         Status = "Pending" // Set the status to "Pending"
+                     };
+
+                     db.Orders.Add(order);
+
+                     // Save the order
+                     db.SaveChanges();
+
+
+                 }
+
+                 var selectedProductsJson = JsonConvert.SerializeObject(selectedProducts);
+                 var cookie = new HttpCookie("SelectedProducts", selectedProductsJson);
+                 Response.Cookies.Add(cookie);
+             }
+
+             return RedirectToAction("Confirmation", "Customer");
+         }*/
+        public ActionResult BuyProduct(FormCollection form)
+        {
+           /* Dictionary<int, SelectedProduct> selectedProducts = new Dictionary<int, SelectedProduct>();
+            int customerId = 0;
+            if (Request.Cookies["CustomerInfo"] != null)
+            {
+                customerId = int.Parse(Request.Cookies["CustomerInfo"]["CustomerId"]);
+            }
+            foreach (var key in form.AllKeys)
+            {
+                if (key.StartsWith("selectedProducts[") && key.EndsWith("].IsSelected"))
+                {
+                    int productId = int.Parse(key.Split('[')[1].Split(']')[0]);
+                    int quantity = int.Parse(form[key]);
+                    bool isSelected = form[key] == "true";
+
+                }
+            }*/
+
+
+            return RedirectToAction("Confirmation"); 
+        }
+
+
+
+
+
+
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public ActionResult SlectedProducts()
+        {
+            var selectedProductsCookie = Request.Cookies["SelectedProducts"];
+            List<SelectedProduct> selectedProducts = new List<SelectedProduct>();
+
+            if (selectedProductsCookie != null)
+            {
+                string selectedProductsJson = selectedProductsCookie.Value;
+                selectedProducts = JsonConvert.DeserializeObject<List<SelectedProduct>>(selectedProductsJson);
+            }
+
+            // Now, you have the selected products from the cookie.
+
+            return View(selectedProducts);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ProductManagementSystem/ProductManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using ProductManagementSystem.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ProductManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string UserAuthenticationCookieName = "UserAuthentication";
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customer login)
+        {
+            var db = new ProductManagementEntities();
+            var res = db.Customers.FirstOrDefault(c => c.Username == login.Username && c.Password == login.Password);
+            if (res != null)
+            {
+                var cookie = new HttpCookie("CustomerInfo");
+                cookie["Username"] = res.Username;
+                cookie["Password"] = res.Password;
+                cookie["CustomerId"] = res.Id.ToString();
+                cookie.Expires = DateTime.Now.AddMinutes(5);
+                Response.Cookies.Add(cookie);
+
+                //TempData["Message"] = "Valid";
+                //return View();
+                 if (login.Username == "Admin")
+                 {
+                     return RedirectToAction("Index", "Admin");
+                 }
+                 else
+                 {
+                    var customer = db.Customers.FirstOrDefault(c => c.Username == login.Username);
+                    ViewBag.CustomerId = customer.Id;
+                    return RedirectToAction("Index", "Customer", new { customerId = customer.Id });
+                 }
+            }
+            else
+            {
+                TempData["Message"] = "Invalid username or password.";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            // Clear the "CustomerInfo" cookie
+            if (Request.Cookies["CustomerInfo"] != null)
+            {
+                var customerCookie = new HttpCookie("CustomerInfo");
+                customerCookie.Expires = DateTime.Now.AddYears(-1); 
+                Response.Cookies.Add(customerCookie);
+            }
+
+            return RedirectToAction("Login"); 
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
